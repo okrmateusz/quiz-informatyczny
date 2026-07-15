4f896ccf0da85612a19e56e111e448771014274d
Mateusz Okruch <okr.mateusz@gmail.com>
2026-07-16 00:29:01 +0200

    init

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = "quiz-v1";
+const FILES = ["./", "index.html", "config.json", "questions.json", "manifest.webmanifest"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
